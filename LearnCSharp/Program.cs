9da60974b3a40c0ab135fff33ab8472a508d3f2d
Chris8018 <tranuytrieuvi@protonmail.com>
2019-07-29 18:08:33 -0700

    Rollback

--- LearnCSharp/Program.cs
+++ LearnCSharp/Program.cs
@@ -6,24 +6,7 @@ namespace LearnCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Time for some class/object testing!!");
-            Console.WriteLine("Why the fuck does will have to have a Main function? that sound so fucking wrong");
-            var testClass = new TestClass();
-            testClass.show();
-        }
-    }
-
-    class TestClass
-    {
-        public TestClass()
-        {
-            // None
-        }
-
-        public void show()
-        {
-            Console.WriteLine("This is a test!");
+            Console.WriteLine("This is a test");
         }
     }
 }
