d2ef91f0ce544b705c51ffcde767e5b2ef54c399
Chris8018 <tranuytrieuvi@protonmail.com>
2019-07-29 23:46:53 -0700

    Done the implementation for MVC test

--- MVC/Form1.cs
+++ MVC/Form1.cs
@@ -10,11 +10,63 @@ using System.Windows.Forms;
 
 namespace MVC
 {
-    public partial class Form1 : Form
+    /*
+     * form is really the view component which will implement imodelobserver interface
+     * so that, it can invoke the valueincremented function which is the implementation
+     * form also implements the iview interface to send the view changed event and to
+     * set the controller associated with the view
+     */
+    public partial class Form1 : Form, IModelObserver, IView
     {
+        IController controller;
+        public event ViewHandler<IView> changed;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /*
+         *  view will set the associated controller, this is how view is link to the controller
+         */
+        public void setController(IController controller)
+        {
+            this.controller = controller;
+        }
+
+        /**
+         * this event is implementation from imodelobserver which will be invoke by the
+         * model when there is a change in the value with modeleventargs which is derived
+         * from the eventargs. the imodel object is the one from which invoke this.
+         */
+        public void valueIncremented(IModel model, ModelEventArgs e)
+        {
+            textBox1.Text = "" + e.newval;
+        }
+
+        /*
+         * when user clicks button just ask the controller to increment the value
+         * do not worry about how and where it is done
+         */
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            controller.incvalue();
+        }
+
+        // when this event is raised can mean the user must have changed the value.
+        // invoke the view changed event in the contrller which will call the method
+        // in imodel to set the new value, user can enter a new value and the invrementing starts
+        // from that value onwards
+        private void TextBox1_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                changed.Invoke(this, new ViewEventArgs(int.Parse(textBox1.Text)));
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please enter a valid number");
+            }
         }
     }
 }
--- MVC/Program.cs
+++ MVC/Program.cs
@@ -16,7 +16,17 @@ namespace MVC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            // create view, model, controller obj
+            // create a controller obj with the concreate implementation inccontroller
+            // and pass the view and model
+            // controller will store its associated model and view inside the constructor
+            // view which is inside the form will tie the controller automatically
+            // run the view obj created by you which is infact the form
+            Form1 view = new Form1();
+            IModel model = new IncModel();
+            IController controller = new IncController(view, model);
+            Application.Run(view);
         }
     }
 }
